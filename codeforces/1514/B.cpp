30b1f555de5be9cc1c5c537941e4681db6596450
Sumit Kumar <4198sumit@gmail.com>
2021-04-21 00:09:00 +0530

    Add solution for problem `B - AND 0, Sum Big` Link: https://codeforces.com/contest/1514/problem/B Tags: bitmasks, combinatorics, math, *1200 Ref: https://codeforces.com/contest/1514/submission/113644638

---
+++ codeforces/1514/B.cpp
@@ -0,0 +1,75 @@
+// J6Y4FQS 1a,
+#include<bits/stdc++.h>
+
+using namespace std; 
+#define int long long int
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(0);
+// #define endl "\n"
+#define mod 1000000007
+#define N 200002
+#define N1 4000
+// #define mod 1000000009
+//============
+#define parr(arr) for(auto &i:arr) cerr<<i<<" "; cerr<<endl;
+#define pvar(a) cerr<<a<<endl;
+#define pset(time) for(auto it=time.begin(); it!=time.end();it++) cerr<< *it <<" "; cerr<<endl;
+#define pvact(v,n)  for (int x=0;x<n;x++) cerr << v[x] << " ";  cerr<<endl;
+#define pmap(m)  for(auto it=m.begin();it!=m.end();it++) cerr<< it->first <<"->"<<it->second <<endl;
+//=============
+void solve();
+
+
+void vok()
+{
+    #ifndef ONLINE_JUDGE 
+    freopen("input1.txt", "r", stdin); 
+    freopen("error.txt", "w", stderr); 
+    freopen("output.txt", "w", stdout); 
+    #endif 
+}
+
+int32_t  main() 
+{
+    fastio
+
+    vok();
+
+    // pmap(m)
+    int t=1; 
+    cin>>t;  
+    for(int i=1;i<=t;i++) 
+    { 
+        // cerr<<"\n======"<<i<<"=======\n";
+        // cout<<"Case #"<<i<<": ";
+        solve();
+        cout<<endl;         
+    }    
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+    return 0; 
+} 
+
+
+// vector<pair<int,int>>a(N);
+
+// string str="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::vector<int> a(N);
+// std::vector<int> b(N);
+
+
+void solve() 
+{
+   int n,k;
+   cin>>n>>k;
+   int ans=1;
+
+  for(int i=0;i<k;i++)
+  {
+    ans= (ans%mod * n%mod) %mod;
+
+  }
+  cout<<ans;
+
+
+   
+} 
