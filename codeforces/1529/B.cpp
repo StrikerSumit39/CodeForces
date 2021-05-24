64dfe55a9c52097692bed309db9d602b538dd39c
Sumit Kumar <4198sumit@gmail.com>
2021-05-24 21:28:00 +0530

    Add solution for problem `B - Sifid and Strange Subsequences` Link: https://codeforces.com/contest/1529/problem/B Tags: greedy, math, sortings, *1100 Ref: https://codeforces.com/contest/1529/submission/117234568

---
+++ codeforces/1529/B.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+//===================================tempelet=======================================
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(0);
+#define endl "\n"
+#define int long long int
+
+#define ll long long int
+#define ull unsigned long long int
+#define db double
+#define comp bool comp(ll x, ll y){return x>y;}
+#define pb push_back
+#define For(a,n,xx) for(long xx=a;xx<n;xx++)
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvc vector<vc>
+#define umap unordered_map
+//======================debug===============================================
+#define parr(arr) for(auto &i:arr) cerr<<i<<" "; cerr<<endl;
+#define pvar(a) cerr<<a<<endl;
+#define pset(time) for(auto it=time.begin(); it!=time.end();it++) cerr<< *it <<" "; cerr<<endl;
+#define pvact(v,n)  for (int x=0;x<n;x++) cerr << v[x] << " "; cerr<<endl;
+#define pmap(m)  for(auto it=m.begin();it!=	m.end();it++) cerr<< it->first <<"->"<<it->second <<endl;
+//===========================================================================  
+void solve();
+
+#define N 300002
+#define N1 1001
+#define  mod 1000000007
+//==========================================================================
+void vok()
+{
+	#ifndef ONLINE_JUDGE 
+	freopen("input2.txt", "r", stdin); 
+	freopen("error2.txt", "w", stderr); 
+	freopen("outpu2.txt", "w", stdout); 
+	#endif 
+}
+
+
+ 
+int32_t  main() 
+{
+	fastio
+
+	// vok();
+
+	
+	
+	// pmap(m)
+	int t=1; 
+	cin>>t;  
+	for(int i=1;i<=t;i++) 
+	{ 
+		cerr<<"\n======"<<i<<"=======\n";
+		// cout<<"Case #"<<i<<": ";
+		solve() ;
+		cout<<endl;			
+ 	}	 
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+} 
+
+std::vector<int> a(N);
+void solve() 
+{
+	int n;
+	cin>>n;
+	
+	int c=0;
+	int md=INT_MAX;
+	int fp=-1,fd=0;
+	sort(a.begin(),a.begin()+n);
+	// std::vector<<int,int>> v;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+				
+	}
+
+	sort(a.begin(),a.begin()+n);
+
+	int i;
+	for( i=0;i<n;i++)
+	{	
+		if(i>0)
+		md= min(md,a[i]-a[i-1]);
+		if(a[i]<=0)
+		c++;
+		else
+			break;
+
+		
+	}
+	// cerr<<c<<endl;
+	for(i;i<n;i++)
+	{
+		if(a[i]<=md)
+		{
+			c++;
+			break;
+		}
+	}
+
+
+	cout<<c;
+	
+	
+
+
+} 
