35edf179d1b249f71e5a3c5551bbb9c06d6d9e1e
Sumit Kumar <4198sumit@gmail.com>
2021-08-01 21:15:00 +0530

    Add solution for problem `B - Gregor and the Pawn Game` Link: https://codeforces.com/contest/1549/problem/B Tags: dfs and similar, dp, flows, graph matchings, graphs, greedy, implementation, *800 Ref: https://codeforces.com/contest/1549/submission/124572736

---
+++ codeforces/1549/B.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+//===================================tempelet=======================================
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(0);
+#define endl "\n"
+#define int long long int
+// enum alp{S};
+#define ll long long int
+#define ull unsigned long long int
+#define db double
+// #define comp bool comp(ll x, ll y){return x>y;}
+#define pb push_back
+#define For(a,n,xx) for(long xx=a;xx<n;xx++)
+#define vi vector<int>
+#define vc vector<char>
+#define vll vector<ll> 
+#define vvi vector<vi>
+#define pii pair<int,int>
+#define vvc vector<vc>
+#define umap unordered_ map
+//======================debug===============================================
+#define parr(arr) for(auto &i:arr) cerr<<i<<" "; cerr<<endl;
+#define pvar(a) cerr<<a<<endl;
+#define pset(time) for(auto it=time.begin(); it!=time.end();it++) cerr<< *it <<" "; cerr<<endl;
+#define pvact(v,n)  for (int x=0;x<n;x++) cerr << v[x] << " "; cerr<<endl;  
+#define pmap(m)  for(auto it=m.begin();it!=	m.end();it++) cerr<< it->first <<"->"<<it->second <<endl;
+
+
+//===========================================================================  
+void solve();
+
+#define N 600002
+#define N1 400001
+#define  mod 1000000007
+//==========================================================================
+void vok()
+{
+	#ifndef ONLINE_JUDGE 
+	freopen("input2.txt", "r", stdin); 
+	freopen("error2.txt", "w", stderr); 
+	freopen("outpu2.txt", "w", stdout); 
+	#endif 
+}
+bool comp(pair<string, int>& a,
+         pair<string, int>& b)
+{
+	if(a.second < b.second)
+    return a.second < b.second;
+	else if(a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	else
+		return a.second < b.second;
+
+
+}
+
+
+int32_t  main() 
+{
+	fastio
+
+	vok();
+
+	// pmap(m)
+	int t=1; 
+	cin>>t;  
+
+	for(int i=1;i<=t;i++) 
+	{ 
+		// cerr<<"\n======"<<i<<"=======\n";
+		// cout<<"Case #"<<i<<": ";
+		solve() ;
+		cout<<endl;			
+ 	}	 
+	cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl; 
+	return 0; 
+} 
+
+  
+
+
+void solve() 
+{
+
+	int n;
+	cin>>n;
+	
+	string a,b;
+	cin>>a>>b;
+
+	int ans=0;
+	for(int i=0;i<n;i++)
+	{
+		if(b[i]=='1' and a[i]=='0')
+		{
+			ans++;
+			b[i]='0';
+			a[i]='-';
+		}
+
+		if(b[i]=='1' and (a[i]=='1'|| a[i]=='-'))
+		{
+			if(i-1>-1)
+			if(  a[i-1]=='1' and b[i]=='1'  )
+			{
+				ans++;
+				a[i-1]='-';
+				b[i]='0';
+			}
+
+			if(i+1<n)
+			if(a[i+1]=='1' and b[i]=='1')
+			{
+				ans++;
+				a[i+1]='-';
+				b[i]='0';
+			}
+		}
+		// cerr<<a<<endl<<b<<endl<<endl;
+	}
+	// for(int i=0;i<n;i++)
+	// {
+	// 	if(b[i]=='1' and a[i]=='0')
+	// 	{
+	// 		ans++;
+	// 		a[i]='-';
+	// 	}
+	// }
+	
+
+	// cerr<<a<<endl<<b<<endl;
+	cout<<ans;
+
+
+	
+
+
+
+} 
+
+// int n;
+// 	cin>>n;
+
+// 	if(n%2)
+// 	{
+// 		cout<<2<<" "<<n-1;
+// 	}
+// 	else
+// 	{
+// 		cout<<2<<" "<<n/2;
+// 	}
